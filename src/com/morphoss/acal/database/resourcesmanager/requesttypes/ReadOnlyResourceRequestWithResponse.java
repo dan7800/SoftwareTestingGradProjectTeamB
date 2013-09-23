0804b44ae9abc99c8aa29a73b01115061ccf71eb
Andrew McMillan <andrew@morphoss.com>
2013-09-23 15:10:11 +0200

    Try to get a bit more information when an exception happens here.

--- src/com/morphoss/acal/database/resourcesmanager/requesttypes/ReadOnlyResourceRequestWithResponse.java
+++ src/com/morphoss/acal/database/resourcesmanager/requesttypes/ReadOnlyResourceRequestWithResponse.java
@@ -1,5 +1,7 @@
 package com.morphoss.acal.database.resourcesmanager.requesttypes;
 
+import android.util.Log;
+
 import com.morphoss.acal.database.resourcesmanager.ResourceResponse;
 import com.morphoss.acal.database.resourcesmanager.ResourceResponseListener;
 
@@ -10,7 +12,7 @@ public abstract class ReadOnlyResourceRequestWithResponse<E> implements	ReadOnly
 	private ResourceResponseListener<E> callBack = null;
 	private int priority = 5;
 	private boolean processed = false;
-	
+
 	public static final int MAX_PRIORITY = 10;
 	public static final int MIN_PRIORITY = 1;
 	/**
@@ -21,7 +23,7 @@ public abstract class ReadOnlyResourceRequestWithResponse<E> implements	ReadOnly
 	protected ReadOnlyResourceRequestWithResponse(ResourceResponseListener<E> callBack ){
 		this.callBack = callBack;
 	}
-	
+
 	public ReadOnlyResourceRequestWithResponse(ResourceResponseListener<E> callBack, int priority)  {
 		this(callBack);
 		if (priority < MIN_PRIORITY || priority > MAX_PRIORITY) throw new IllegalArgumentException("Priority out of bounds"+priority);
@@ -32,8 +34,8 @@ public abstract class ReadOnlyResourceRequestWithResponse<E> implements	ReadOnly
 	public boolean isProcessed() { return this.processed; }
 	@Override
 	public synchronized void setProcessed() { this.processed = true; }
-	
-	
+
+
 	/**
 	 * Called by child classes to send response to the callback. Sends response on its own Thread so will usually return immediately.
 	 * Beware of Race conditions when sending multiple requests - callbacks may come back in an arbitrary order.
@@ -46,16 +48,24 @@ public abstract class ReadOnlyResourceRequestWithResponse<E> implements	ReadOnly
 			@Override
 			public void run() {
 				ReadOnlyResourceRequestWithResponse.this.setProcessed();
-				callBack.resourceResponse(response);
+				try {
+				    callBack.resourceResponse(response);
+				}
+				catch( Exception e) {
+				    Log.w("aCal ReadOnlyResourceRequestWithResponse",
+				            "Request class is: "+this.getClass().getSimpleName()
+				            +", Response class is: "+response.getClass().getName()
+				            +" Exception:\n"+Log.getStackTraceString(e));
+				}
 			}
 		}).start();
 	}
-	
+
 	@Override
 	public int compareTo(ReadOnlyResourceRequest r) {
 		return this.priority - r.priority();
 	}
-	
+
 	@Override
 	public int priority() {
 		return this.priority;
