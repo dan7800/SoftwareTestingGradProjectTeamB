19617e3221f1aafabadc89a531627125850a861c
dkubarycz <dkubarycz@gmail.com>
2015-04-05 12:52:56 -0400

    Adding aCal source code and AndroLint XML results.

---
+++ apps/acal/acal-acal/src/com/morphoss/acal/acaltime/UnsupportedRepetitionException.java
@@ -0,0 +1,11 @@
+package com.morphoss.acal.acaltime;
+
+public class UnsupportedRepetitionException extends RuntimeException {
+
+    public UnsupportedRepetitionException(String msg) {
+        super(msg);
+    }
+
+    private static final long   serialVersionUID    = 1L;
+
+}
